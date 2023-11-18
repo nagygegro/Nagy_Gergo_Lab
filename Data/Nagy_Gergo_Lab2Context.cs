ccde3953a07a499c8bbedd14a524389bdb1dff5b
Nagy Gergo <nagy_gerg@yahoo.com>
2023-11-18 10:01:13 +0200

    Add project files.

---
+++ Data/Nagy_Gergo_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Nagy_Gergo_Lab2.Models;
+
+namespace Nagy_Gergo_Lab2.Data
+{
+    public class Nagy_Gergo_Lab2Context : DbContext
+    {
+        public Nagy_Gergo_Lab2Context (DbContextOptions<Nagy_Gergo_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Nagy_Gergo_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Nagy_Gergo_Lab2.Models.Publisher>? Publisher { get; set; } = default!;
+
+        public DbSet<Nagy_Gergo_Lab2.Models.Author>? Author { get; set; }
+    }
+}
---
+++ Models/Author.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Nagy_Gergo_Lab2.Models
+{
+    public class Author
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [NotMapped]
+        public string FullName { get => $"{FirstName} {LastName}"; set => FullName = value; }
+    }
+}
